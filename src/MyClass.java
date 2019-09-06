2149372b64e366f1b226d51dbbf4b1c79b063ae4
Nicki Hvolgaard <38239769+TheMagicalKing@users.noreply.github.com>
2019-09-06 17:21:39 +0200

    Tasks from the book finished

--- src/MyClass.java
+++ src/MyClass.java
@@ -10,13 +10,20 @@ class MyClass {
         for (int x = 1; x <= 5; x++) {
             System.out.println("I love java");
         }
+        //1.4
         System.out.println("a   a^2     a^3     a^4");
         System.out.println("1    1       1       1");
         System.out.println("2    4       8      16");
         System.out.println("3    9       27     87");
         System.out.println("4    16      64     256");
-
+        //1.5
         System.out.println((7.5*6.5-4.5*3)/(47.5-5.5));
+
+        var sum = 1+2+3+4+5+6+7+8+9+10;
+        System.out.println(sum);
+
+        var sum2 = 24/1.6;
+        System.out.println(sum2);
 
     }
 }
