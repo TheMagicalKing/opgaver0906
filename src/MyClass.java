94c0605de81b0bf9f380c5543eeb007908341734
Nicki Hvolgaard <38239769+TheMagicalKing@users.noreply.github.com>
2019-09-06 13:49:54 +0200

    Initial commit

---
+++ src/MyClass.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class MyClass {
+    public static void main(String[ ] args) {
+        //1.1
+        System.out.println("Programming is fun!");
+        System.out.println("Learning Java now");
+        System.out.println("Fundamentals First");
+        //1.2
+        for (int x = 1; x <= 5; x++) {
+            System.out.println("I love java");
+        }
+        System.out.println("a   a^2     a^3     a^4");
+        System.out.println("1    1       1       1");
+        System.out.println("2    4       8      16");
+        System.out.println("3    9       27     87");
+        System.out.println("4    16      64     256");
+
+        System.out.println((7.5*6.5-4.5*3)/(47.5-5.5));
+
+    }
+}
